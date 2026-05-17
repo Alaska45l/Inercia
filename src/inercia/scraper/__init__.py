4523ee971703996958c7141300406e78a077910d
alaska45l <alaska45l@hotmail.com>
2026-05-17 15:22:34 -0300

    Fix production app startup

--- src/inercia/scraper/__init__.py
+++ src/inercia/scraper/__init__.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
 
 from inercia.scraper.feed import FeedJob, fetch_job_urls, fetch_jobs, parse_job_urls, parse_rss
-from inercia.scraper.filter_scraper import FilteredJobCard, discover_filtered_jobs
 from inercia.scraper.job_detail import JobMarkdown, extract_job_markdown
 
 __all__ = [
     "FeedJob",
-    "FilteredJobCard",
     "JobMarkdown",
-    "discover_filtered_jobs",
     "extract_job_markdown",
     "fetch_job_urls",
     "fetch_jobs",
